4290bc0e9b5812b5b62be7aa1a13937b3411608d
clnhrn <herniacln@gmail.com>
2026-06-08 17:25:38 +0800

    feat: add starter calculator.py file

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""
+This module serves as the starter file for the Git Collaboration Challenge.
+Group members will simultaneously add their assigned arithmetic functions
+below the designated TODO comment to practice resolving merge conflicts.
+"""
+
+def welcome_message():
+    print("Welcome to the Collaboration Calculator!")
+
+# ==========================================
+# TODO: Add your functions below this line
+# ==========================================
+
+
+if __name__ == "__main__":
+    welcome_message()
